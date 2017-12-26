3b267762440bda00a298ee40ea84a3eef853e20c
Raphael Amorim <rapha850@gmail.com>
2017-12-26 19:25:15 -0200

    gh-pages

---
+++ webpack.config.babel.js
@@ -0,0 +1,84 @@
+import path from 'path'
+import webpack from 'webpack'
+
+const sourcePath = path.join(__dirname);
+
+let distConfig = {
+  entry: path.resolve(sourcePath, 'src/index.js'),
+  filename: 'reactsandbox.js'
+}
+
+if (process.env.NODE_ENV === 'DEV_SERVER') {
+  distConfig.entry = path.resolve(sourcePath, 'assets/example.js')
+  distConfig.filename = 'bundle.js'
+}
+
+const config = {
+  target: 'web',
+  entry: distConfig.entry,
+  externals: {
+    // Use external version of React
+    "react": "React"
+  },
+  output: {
+    path: __dirname,
+    library: 'reactsandbox',
+    libraryTarget: 'umd',
+    umdNamedDefine: true,
+    filename: distConfig.filename
+  },
+  resolve: {
+    alias: {
+      react: path.resolve(__dirname, '../../node_modules/react'),
+    },
+    extensions: ['.js'],
+    modules: [
+      sourcePath,
+      path.resolve(__dirname, 'node_modules'),
+
+      // yarn workspaces
+      path.resolve(__dirname, '../../node_modules'),
+    ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader'
+        ],
+        include: sourcePath
+      }
+    ],
+  },
+  plugins: [],
+}
+
+if (process.env.NODE_ENV === 'PROD') {
+  config.plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      mangle: true,
+      compress: {
+        warnings: false, // Suppress uglification warnings
+        pure_getters: true,
+        unsafe: true,
+        unsafe_comps: true,
+        screw_ie8: true
+      },
+      output: {
+        comments: false,
+      },
+    })
+  )
+  config.plugins.push(new webpack.NoEmitOnErrorsPlugin())
+  config.plugins.push(
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    })
+  )
+  config.plugins.push(new webpack.optimize.ModuleConcatenationPlugin())
+  config.plugins.push(new webpack.HashedModuleIdsPlugin())
+}
+
+export default config;
